0d228c64d024ea785492ecdb7a20a06c0823d603
Yurii_Yevchenko <yurievch@gmail.com>
2023-11-15 18:52:16 +0200

    в css or js сделал закрытие и открытие крестика menu__btn

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ $(function () {
   var mixer = mixitup(".gallery__items");
 });
 
-$(".menu__btn").on("click", function () {
-  $(".menu__list").toggleClass("menu__list--active");
+document.querySelector(".menu__btn").addEventListener("click", function () {
+  this.classList.toggle("menu__list--active");
+  document.querySelector(".menu__list").classList.toggle("menu__list--active");
 });
+
+// $(".menu__btn").on("click", function () {
+//   $(".menu__list").toggleClass("menu__list--active");
+// });
