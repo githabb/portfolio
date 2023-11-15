e107f8ff84b6211c20b399f48cb6481af3a2ccc1
Yurii_Yevchenko <yurievch@gmail.com>
2023-11-15 15:58:00 +0200

    добавил файл media.css, написал код js для бургера, написал адаптив для header, main, skills, services.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function () {
   var mixer = mixitup(".gallery__items");
 });
+
+$(function () {
+  var mixer = mixitup(".gallery__items");
+});
+
+$(".menu__btn").on("click", function () {
+  $(".menu__list").toggleClass("menu__list--active");
+});
