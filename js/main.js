4a55ed2762096d22afc51f8083cc0f83468452de
Yurii_Yevchenko <yurievch@gmail.com>
2023-11-22 18:32:58 +0200

    адаптив не работает

--- js/main.js
+++ js/main.js
@@ -10,7 +10,3 @@ document.querySelector(".menu__btn").addEventListener("click", function () {
   this.classList.toggle("menu__list--active");
   document.querySelector(".menu__list").classList.toggle("menu__list--active");
 });
-
-// $(".menu__btn").on("click", function () {
-//   $(".menu__list").toggleClass("menu__list--active");
-// });
