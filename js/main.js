217f6d005ea92b0f07115d402e3a8f7f4b7ed5db
Yurii_Yevchenko <yurievch@gmail.com>
2024-01-05 14:23:16 +0200

    добавил липкий хедер, переход поссылкам и переход в социальные сети

--- js/main.js
+++ js/main.js
@@ -2,11 +2,23 @@ $(function () {
   var mixer = mixitup(".gallery__items");
 });
 
-$(function () {
-  var mixer = mixitup(".gallery__items");
-});
-
 document.querySelector(".menu__btn").addEventListener("click", function () {
   this.classList.toggle("active");
   document.querySelector(".menu__list").classList.toggle("menu__list--active");
 });
+
+document.querySelector(".menu__list").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".menu__list").classList.toggle("menu__list--active");
+});
+
+var header = document.querySelector(".menu");
+var stickyOffset = header.offsetTop;
+
+window.onscroll = function showHeader() {
+  if (window.scrollY > stickyOffset) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
